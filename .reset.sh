d5cc1f88d8487621f948bb23ea56c255bd1deac1
arnaud_pesenti <arnaud_pesenti@yahoo.fr>
2024-10-13 15:52:10 +0000

    UPDATES OF .reset.sh & .gitignore files

--- .reset.sh
+++ .reset.sh
@@ -4,7 +4,7 @@ echo ">>> All automatically created files and/or directories will be deleted."
 
 rm -rf .terraform
 rm -f  ./.terraform.lock.hcl
-rm -f  terraform.tfstate
-rm -f  terraform.tfstate.backup
+rm -f  ./terraform.tfstate
+rm -f  ./terraform.tfstate.backup
 
 echo ">>> Reset done successfully!"
